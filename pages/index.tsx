c9adc537cb1bcc4a36ed975dda3c5e04910d1353
Reuben Ayres <reuben@spantree.net>
2021-05-03 15:21:34 -0500

    added chitchat example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 // import styles from '../styles/Home.module.css'
 
+import React from "react";
+import Example from "../components/Example";
+
 export default function Home() {
-  return <div>Tailwind App</div>;
+  return (
+    <>
+      <div>Example from Docs</div>
+      <Example />
+    </>
+  );
 }
